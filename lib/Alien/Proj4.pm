0ed3f0105854d503bec8c14752fc4582272d0881
Shawn Laffan <shawnlaffan@gmail.com>
2019-04-27 08:55:36 +1000

    use Alien::Base methods to build projection info

--- lib/Alien/Proj4.pm
+++ lib/Alien/Proj4.pm
@@ -6,6 +6,85 @@ use parent qw( Alien::Base );
 
 our $VERSION = '2.019105';
 
+sub installed {1}
+
+# dup of code currently in PDLA::GIS::Proj
+sub load_projection_descriptions {
+  my ($class) = @_;
+  my $incflags = $class->cflags;
+  my $libflags = $class->libs;
+  
+  require Inline;
+  Inline->bind(C => <<'EOF', inc => $incflags, libs => $libflags) unless defined &list_projections;
+#include "projects.h"
+HV *list_projections() {
+  struct PJ_LIST *lp;
+  SV* scalar_val;
+  HV *hv = newHV();
+  for (lp = pj_get_list_ref() ; lp->id ; ++lp) {
+      scalar_val  = newSVpv( *lp->descr, 0 );
+      hv_store( hv, lp->id, strlen( lp->id ), scalar_val, 0 );
+  }
+  return hv;
+}
+EOF
+  list_projections();
+}
+
+# dup of code currently in PDLA::GIS::Proj
+sub load_projection_information {
+    my ($class) = @_;
+    my $descriptions = $class->load_projection_descriptions();
+    my $info = {};
+    foreach my $projection ( keys %$descriptions ) {
+        my $description = $descriptions->{$projection};
+        my $hash = {};
+        $hash->{CODE} = $projection;
+        my @lines = split( /\n/, $description );
+        chomp @lines;
+        # Full name of this projection:
+        $hash->{NAME} = $lines[0];
+        # The second line is usually a list of projection types this one is:
+        my $temp = $lines[1];
+        $temp =~ s/no inv\.*,*//;
+        $temp =~ s/or//;
+        my @temp_types = split(/[,&\s]/, $temp );
+        my @types = grep( /.+/, @temp_types );
+        $hash->{CATEGORIES} = \@types;
+        # If there's more than 2 lines, then it usually is a listing of parameters:
+        # General parameters for all projections:
+        $hash->{PARAMS}->{GENERAL} =
+            [ qw( x_0 y_0 lon_0 units init no_defs geoc over ) ];
+        # Earth Figure Parameters:
+        $hash->{PARAMS}->{EARTH} =
+            [ qw( ellps b f rf e es R R_A R_V R_a R_g R_h R_lat_g ) ];
+        # Projection Specific Parameters:
+        my @proj_params = ();
+        if( $#lines >= 2 ) {
+            foreach my $i ( 2 .. $#lines ) {
+                my $text = $lines[$i];
+                my @temp2 = split( /\s+/, $text );
+                my @params = grep( /.+/, @temp2 );
+                foreach my $param (@params) {
+                    $param =~ s/=//;
+                    $param =~ s/[,\[\]]//sg;
+                    next if $param =~ /^and|or|Special|for|Madagascar|fixed|Earth|For|CH1903$/;
+                    push(@proj_params, $param);
+                }
+            }
+        }
+        $hash->{PARAMS}->{PROJ} = \@proj_params;
+        # Can this projection do inverse?
+        $hash->{INVERSE} = ( $description =~ /no inv/ ) ? 0 : 1;
+        $info->{$projection} = $hash;
+    }
+    # A couple of overrides:
+    $info->{ob_tran}->{PARAMS}->{PROJ} =
+        [ 'o_proj', 'o_lat_p', 'o_lon_p', 'o_alpha', 'o_lon_c',
+          'o_lat_c', 'o_lon_1', 'o_lat_1', 'o_lon_2', 'o_lat_2' ];
+    $info->{nzmg}->{CATEGORIES} = [ 'fixed Earth' ];
+    return $info;
+}
 
 
 1;
